54cd3bc5dd856ab4e73928c4ead081d4ad2aa9d2
Frank Li <li.26.frank@gmail.com>
2020-07-02 15:49:15 -0400

    leetcode july 2020 challenge - day 2

---
+++ leetcode/july-2020-challenge/day2/binary_tree_level_order_traversal_ii.py
@@ -0,0 +1,51 @@
+# Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+
+# For example:
+# Given binary tree [3,9,20,null,null,15,7],
+
+#     3
+#    / \
+#   9  20
+#     /  \
+#    15   7
+
+# return its bottom-up level order traversal as:
+
+# [
+#   [15,7],
+#   [9,20],
+#   [3]
+# ]
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        out = list()
+        
+        def helper(level):
+            order = list()
+            next_level = list()
+            
+            for node in level:
+                if node:
+                    order.append(node.val)
+                    next_level.append(node.left)
+                    next_level.append(node.right)
+                    
+            if len(next_level):
+                helper(next_level)
+            
+            if len(order):
+                out.append(order)
+            
+        helper([root])
+        
+        return out
+        
