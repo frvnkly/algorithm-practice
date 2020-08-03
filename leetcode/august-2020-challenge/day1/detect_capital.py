b0e77af8e7559bac11edacc497f33fb60fc0802e
Frank Li <li.26.frank@gmail.com>
2020-08-03 02:28:25 -0400

    leetcode august 2020 challenge - day 1

---
+++ leetcode/august-2020-challenge/day1/detect_capital.py
@@ -0,0 +1,41 @@
+# Given a word, you need to judge whether the usage of capitals in it is right or not.
+
+# We define the usage of capitals in a word to be right when one of the following cases holds:
+
+# All letters in this word are capitals, like "USA".
+# All letters in this word are not capitals, like "leetcode".
+# Only the first letter in this word is capital, like "Google".
+# Otherwise, we define that this word doesn't use capitals in a right way.
+
+# Example 1:
+
+# Input: "USA"
+# Output: True
+
+# Example 2:
+
+# Input: "FlaG"
+# Output: False
+
+# Note: The input will be a non-empty word consisting of uppercase and lowercase latin letters.
+
+
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        first_char_lowercase = not self.is_capital(word[0])
+        if first_char_lowercase and len(word) >= 2:
+            if self.is_capital(word[1]):
+                return False
+            
+        if len(word) >= 2:
+            rest_should_be_capital = self.is_capital(word[1])
+            
+            for i in range(2, len(word)):
+                if self.is_capital(word[i]) != rest_should_be_capital:
+                    return False
+                
+        return True
+                    
+    def is_capital(self, c):
+        return c >= 'A' and c <= 'Z'
+        
